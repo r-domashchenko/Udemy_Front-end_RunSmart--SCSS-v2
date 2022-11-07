9fede17b62a62aa6dd0d848cf4409993c93e0023
r-domashchenko <therostofan@gmail.com>
2022-11-07 11:40:49 +0200

    Tabs added. Cards added with script. Adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
-// ADD Jquery library
+// ADD jQuery library
 $(document).ready(function () {
     // work only after document (HTML page) loaded
+
     // method to load SlickSlider
     $('.carousel__inner').slick({
         speed: 1000,
@@ -19,4 +20,43 @@ $(document).ready(function () {
             }
         }]
     });
+
+    //jQuerry tabs 
+    $('ul.catalogue__tabs').on('click', 'li:not(.catalogue__tab_active)', function () {
+        $(this)
+            .addClass('catalogue__tab_active').siblings().removeClass('catalogue__tab_active')
+            .closest('div.container').find('div.catalogue__content').removeClass('catalogue__content_active').eq($(this).index()).addClass('catalogue__content_active');
+    });
+
+    // // Свій скрипт для карток товару (кнопка "подробнее")
+    // $('.catalogue-item__link').each(function (i) {
+    //     $(this).on('click', function (e) {
+    //         e.preventDefault();
+    //         $('.catalogue-item__content').eq(i).toggleClass('catalogue-item__content_active');
+    //         $('.catalogue-item__more').eq(i).toggleClass('catalogue-item__more_active');
+    //     })
+    // });
+    // // Свій скрипт для карток товару (кнопка "назад")
+    // $('.catalogue-item__back').each(function (i) {
+    //     $(this).on('click', function (e) {
+    //         e.preventDefault();
+    //         $('.catalogue-item__content').eq(i).toggleClass('catalogue-item__content_active');
+    //         $('.catalogue-item__more').eq(i).toggleClass('catalogue-item__more_active');
+    //     })
+    // });
+
+    // Оптимізований скрипт (своя функція, яка визиває додаткову інформацію на карточках товару)
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalogue-item__content').eq(i).toggleClass('catalogue-item__content_active');
+                $('.catalogue-item__more').eq(i).toggleClass('catalogue-item__more_active');
+            })
+        });
+    };
+
+    // Визиваємо функцію для кнопки "подробнее" та "назад"
+    toggleSlide('.catalogue-item__link');
+    toggleSlide('.catalogue-item__back');
 });
